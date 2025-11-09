4867e1191d4f775ea94267a6b75f9a89fab4668e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-11-08 23:17:21 -0300

    att

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 local function api_handler(request)
   -- Process the request here
 
-  if request.route == "/api/obtem_devedor" then
+  if request.route == "/api/obtem_devedor_por_cpf" then
 
     local devedor = request.params["devedor"]
     if not devedor then 
@@ -12,21 +12,13 @@ local function api_handler(request)
     local filename = "data/" .. devedor .. ".json"
     return serjao.send_file(filename, "application/json")
   end 
+  
 
-
-  if request.route == "/" then
-    return serjao.send_file("static/paginas/principal.html", "text/html")
-  end
-
-  local page = "static/paginas/".. request.route ..".html"
-  if not dtw.isfile(page) then 
-    return serjao.send_text("Página não encontrada", 404)
-  end
-  return serjao.send_file(page, "text/html")
+   return serjao.send_file("index.html", "text/html")
 
 
 
- 
+
 
 
 end
