bed19159be3bbd469fed26f17540ec1b26acfa89
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-11-10 04:07:09 -0300

    att

--- create.lua
+++ create.lua
@@ -1,7 +1,11 @@
 
-local quantity = 50
+local quantity = 1
 CPF = 1
 CNPJ = 2
+
+local cpf_model = dtw.load_file("models/cpf.json")
+local cnpj_model = dtw.load_file("models/cnpj.json")
+
 function generate_randon_value(size)
     local text = ""
     for i = 1, size do
@@ -17,11 +21,49 @@ function get_cnpj()
 end
 
 
+function construct_element(dest,model)
+    llm = newLLM({})
+    local result = {}
+    llm.add_system_prompt("use the function set_fake_data to return the json with fake data")
+    llm.add_system_prompt("construct a json with fake data based on the following model: " .. model)
+-- Define the callback function to handle color change
+    local set_fake_data = function(args)
+        local fake_data = args.fake_data
+        print("Fake Data: " .. fake_data)
+        local parsed = json.load_from_string(args["fake_data"])
+    end
+
+    local parameters = {
+    {
+        name = "fake_data",
+        description = "The json string with fake data based on the model",
+        type = "json string",
+        required = true
+    }
+}
+    llm.add_function("set_fake_data", "se the fake data", parameters, set_fake_data)
+
+    response = llm.generate()
+    print("Response: " .. response)
+    
+end
+
+function construct_cpf()
+    local cpf = get_cpf()
+    construct_element(cpf, cpf_model)
+end
+function construct_cnpj()
+    local cnpj = get_cnpj()
+    construct_element(cnpj, cnpj_model)
+end
+
+
+
 for i = 1, quantity do
     local choice = math.random(1, 2)
     if choice == CPF then
-        print(get_cpf())
+        construct_cpf()
     else
-        print(get_cnpj())
+        construct_cnpj()
     end
 end
